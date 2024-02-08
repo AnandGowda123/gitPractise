f51e1f805c8567ea93b1db9654734a8f4080a169
Yogananda P <yogananda.p@aurigo.com>
2024-02-08 16:59:05 +0530

    new code

---
+++ src/test/java/gitPractise/LaunchBrowserTest4.java
@@ -0,0 +1,20 @@
+package gitPractise;
+
+import org.example.BaseCLass;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LaunchBrowserTest4 extends BaseCLass {
+
+    WebDriver driver;
+
+    @Test
+    public void launchBrowser() throws IOException {
+        driver= initializeDriver();
+        driver.get("https://demoautomation2303.aurigo.net/");
+        System.out.println("added new code");
+        driver.close();
+    }
+}
