57bdcf5d621ec6c53993b8e0910932f20f5d8d22
Yogananda P <yogananda.p@aurigo.com>
2024-02-08 15:28:21 +0530

    removed code

--- src/test/java/gitPractise/LaunchBrowserTest3.java
+++ src/test/java/gitPractise/LaunchBrowserTest3.java
@@ -15,8 +15,6 @@ public class LaunchBrowserTest3 extends BaseCLass {
         driver= initializeDriver();
         driver.get("https://demoautomation2303.aurigo.net/");
         System.out.println("added new code");
-        System.out.println("added second code");
-        System.out.println("added third code");
         driver.close();
     }
 }
